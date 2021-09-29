8882e1dc4e495d6fbe2223b59dd4b279a53342fd
Hyeok Jun Yun <Hyeok Jun Yun@DESKTOP-8LVIHR7>
2021-09-29 18:18:06 +0900

    ch09 추상 클래스의 의미와 구현하는 방법

---
+++ Chapter03/src/ch09/ComputerTest.java
@@ -0,0 +1,11 @@
+package ch09;
+
+public class ComputerTest {
+
+	public static void main(String[] args) {
+//		Desktop desktop = new Desktop();
+		Computer desktop = new Desktop();
+		desktop.display();
+		desktop.typing();
+	}
+}
